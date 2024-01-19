83c9add96835be09fb07d6a55b5ddf130ddd475a
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-01-19 23:49:32 +0200

    Initial Commit

--- Avocado_recipes/View/ContentView.swift
+++ Avocado_recipes/View/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Avocado Recipes")
-        }
-        .padding()
+        ScrollView(.vertical, showsIndicators: false){
+            VStack(alignment: .center, spacing: 20) {
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack (alignment: .top, spacing: 0){
+                        HeaderView()
+                    }//:Hstack
+                }//:ScrollView
+                VStack(alignment: .center, spacing: 20){
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados byt you were afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                }//:Vstack
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+                
+            }//:Vstack
+        }//:ScrollView
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
