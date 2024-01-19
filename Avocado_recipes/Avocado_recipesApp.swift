eb821bf6043a81c22cf2b742c6fbea88fb62b385
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-01-19 23:49:32 +0200

    Initial Commit

---
+++ Avocado_recipes/Avocado_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avocado_recipesApp.swift
+//  Avocado_recipes
+//
+//  Created by Dimitar Angelov on 19.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Avocado_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocado_recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avocado_recipes
+//
+//  Created by Dimitar Angelov on 19.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
